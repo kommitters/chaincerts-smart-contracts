9fb647f24b0c983da701498e91380e9465448914
Odraxs <deroldan98@gmail.com>
2023-03-29 11:20:28 -0500

    Add new function for chaincert struct

--- certs_wallet/src/chaincert.rs
+++ certs_wallet/src/chaincert.rs
@@ -20,3 +20,24 @@ pub struct Chaincert {
     /// A logical indicator that lets know if a `Chaincert` is revoked or not
     pub revoked: bool,
 }
+
+impl Chaincert {
+    #[cfg(not(tarpaulin_include))]
+    fn _new(
+        cid: Bytes,
+        cont_dist: Address,
+        org_id: Bytes,
+        dist_date: u64,
+        exp_date: OptU64,
+        revoked: bool,
+    ) -> Chaincert {
+        Chaincert {
+            cid,
+            cont_dist,
+            org_id,
+            dist_date,
+            exp_date,
+            revoked,
+        }
+    }
+}

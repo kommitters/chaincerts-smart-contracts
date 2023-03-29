4a5d6b290e37dc0c4598d011513493ec1c0e3611
Odraxs <deroldan98@gmail.com>
2023-03-29 12:15:19 -0500

    Add implementation of add and remove org functions

--- certs_wallet/src/lib.rs
+++ certs_wallet/src/lib.rs
@@ -1,9 +1,10 @@
 #![no_std]
+mod access_control_list;
 mod chaincert;
 mod option;
 mod owner;
 mod storage_types;
-use soroban_sdk::{contractimpl, Address, Env};
+use soroban_sdk::{contractimpl, Address, Bytes, Env};
 
 pub struct Wallet;
 
@@ -14,6 +15,18 @@ impl Wallet {
             panic!("This wallet is already initialized");
         }
         owner::write_owner(&env, &owner);
+    }
+
+    /// Add organizations to the ACL
+    pub fn add_org(env: Env, org_id: Bytes) {
+        owner::read_owner(&env).require_auth();
+        access_control_list::add_organization(&env, &org_id)
+    }
+
+    /// Remove organizations from the ACL
+    pub fn rmv_org(env: Env, org_id: Bytes) {
+        owner::read_owner(&env).require_auth();
+        access_control_list::remove_organization(&env, &org_id)
     }
 }
 

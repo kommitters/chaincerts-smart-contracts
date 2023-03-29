711cb8421cd005fbcefabc4ce32eb361e5294cf1
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2023-03-29 15:00:22 -0500

    Improve variable names
    Co-Authored-By: Juan Esteban Nieto Cifuentes <89233604+Jenietoc@users.noreply.github.com>

--- certs_governance/src/metadata.rs
+++ certs_governance/src/metadata.rs
@@ -10,9 +10,9 @@ pub fn read_file_storage(e: &Env) -> Bytes {
     e.storage().get_unchecked(&key).unwrap()
 }
 
-pub fn write_file_storage(e: &Env, d: Bytes) {
+pub fn write_file_storage(e: &Env, file_storage: Bytes) {
     let key = DataKey::FStorage;
-    e.storage().set(&key, &d);
+    e.storage().set(&key, &file_storage);
 }
 
 pub fn read_name(e: &Env) -> Bytes {
@@ -20,9 +20,9 @@ pub fn read_name(e: &Env) -> Bytes {
     e.storage().get_unchecked(&key).unwrap()
 }
 
-pub fn write_name(e: &Env, d: Bytes) {
+pub fn write_name(e: &Env, name: Bytes) {
     let key = DataKey::Name;
-    e.storage().set(&key, &d)
+    e.storage().set(&key, &name)
 }
 
 pub fn read_revocable(e: &Env) -> bool {
@@ -30,9 +30,9 @@ pub fn read_revocable(e: &Env) -> bool {
     e.storage().get_unchecked(&key).unwrap()
 }
 
-pub fn write_revocable(e: &Env, d: bool) {
+pub fn write_revocable(e: &Env, revocable: bool) {
     let key = DataKey::Revocable;
-    e.storage().set(&key, &d)
+    e.storage().set(&key, &revocable)
 }
 
 pub fn read_expiration_time(e: &Env) -> Option<u64> {
@@ -40,14 +40,14 @@ pub fn read_expiration_time(e: &Env) -> Option<u64> {
     e.storage().get_unchecked(&key).unwrap()
 }
 
-pub fn write_expiration_time(e: &Env, d: Option<u64>) {
+pub fn write_expiration_time(e: &Env, expiration_time: Option<u64>) {
     let key = DataKey::ExpTime;
-    e.storage().set(&key, &d)
+    e.storage().set(&key, &expiration_time)
 }
 
-pub fn create_receivers(e: &Env, d: Vec<Address>) {
+pub fn create_receivers(e: &Env, receivers_address: Vec<Address>) {
     let mut receivers: Map<Address, CertData> = map![e];
-    d.iter().for_each(|receiver| {
+    receivers_address.iter().for_each(|receiver| {
         let address: Address = receiver.unwrap();
         let bytes = address.to_raw().get_payload().to_be_bytes();
         let uuid = Uuid::new_v5(&Uuid::NAMESPACE_DNS, &bytes);
@@ -59,9 +59,9 @@ pub fn create_receivers(e: &Env, d: Vec<Address>) {
     e.storage().set(&key, &receivers)
 }
 
-pub fn write_receivers(e: &Env, d: Map<Address, CertData>) {
+pub fn write_receivers(e: &Env, receivers: Map<Address, CertData>) {
     let key = DataKey::Receivers;
-    e.storage().set(&key, &d)
+    e.storage().set(&key, &receivers)
 }
 
 pub fn read_distribution_limit(e: &Env) -> u32 {
@@ -69,7 +69,7 @@ pub fn read_distribution_limit(e: &Env) -> u32 {
     e.storage().get_unchecked(&key).unwrap()
 }
 
-pub fn write_distribution_limit(e: &Env, d: u32) {
+pub fn write_distribution_limit(e: &Env, distribution_limit: u32) {
     let key = DataKey::DistLimit;
-    e.storage().set(&key, &d)
+    e.storage().set(&key, &distribution_limit)
 }

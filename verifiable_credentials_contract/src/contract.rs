9a22d6510ac64dd7db1520e4be8961aea8c9100a
Mario Rodriguez <64106728+MarioRodriguezS@users.noreply.github.com>
2023-11-23 15:04:31 -0500

    Implement initialize basic setup
    Co-authored-by: Juan Manuel Giraldo <juanmagiraldor@users.noreply.github.com>

---
+++ verifiable_credentials_contract/src/contract.rs
@@ -0,0 +1,38 @@
+use crate::error::ContractError;
+use crate::storage;
+
+use crate::vcs_trait::VCsTrait;
+use soroban_sdk::{
+    contract, contractimpl, contractmeta, panic_with_error, Address, Env, Map, Vec,
+};
+
+const LEDGERS_THRESHOLD: u32 = 1;
+const LEDGERS_TO_EXTEND: u32 = 535_000;
+const DEFAULT_AMOUNT: u32 = 20;
+
+contractmeta!(
+    key = "Description",
+    val = "Smart contract for Verifiable Credentials",
+);
+
+#[contract]
+pub struct VCsContract;
+
+#[contractimpl]
+impl VCsTrait for VCsContract {
+    fn initialize(e: Env, admin: Address, amount: Option<u32>) {
+        if storage::has_admin(&e) {
+            panic_with_error!(e, ContractError::AlreadyInitialized);
+        }
+        storage::write_admin(&e, &admin);
+        storage::write_amount(&e, &amount.unwrap_or(DEFAULT_AMOUNT));
+
+        // set initial empty values
+        storage::write_vcs(&e,  &Vec::new(&e));
+        storage::write_revocations(&e,  &Map::new(&e));
+
+        e.storage()
+            .instance()
+            .bump(LEDGERS_THRESHOLD, LEDGERS_TO_EXTEND);
+    }
+}

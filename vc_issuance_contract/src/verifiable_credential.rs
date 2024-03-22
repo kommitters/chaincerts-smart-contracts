724405cfb4055b2cf493decf1170cc93912c339b
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2024-03-22 09:54:03 -0500

    Remove unused base32 module

--- vc_issuance_contract/src/verifiable_credential.rs
+++ vc_issuance_contract/src/verifiable_credential.rs
@@ -1,32 +1,9 @@
 use soroban_sdk::{Env, String, Vec};
 
-use crate::base32;
 use crate::storage;
 
 pub fn add_vc(e: &Env, vc_id: &String, mut vcs: Vec<String>) {
     vcs.push_front(vc_id.clone());
 
     storage::write_vcs(e, &vcs);
-}
-
-pub fn generate_id(e: &Env) -> String {
-    let random_bytes: [u8; 15] = get_random_bytes(e);
-    let mut id = [0u8; 24];
-
-    base32::encode(&mut id, &random_bytes);
-
-    let str_id = core::str::from_utf8(id.as_ref()).unwrap();
-
-    String::from_str(e, str_id)
-}
-
-fn get_random_bytes(e: &Env) -> [u8; 15] {
-    let mut random_bytes = [0u8; 15];
-
-    for byte in &mut random_bytes {
-        let rand_number: u64 = e.prng().gen_range(0..256);
-        *byte = rand_number as u8;
-    }
-
-    random_bytes
 }
